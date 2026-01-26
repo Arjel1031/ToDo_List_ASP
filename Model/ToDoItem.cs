8446b71b25218a1dba807f351d6474dd169f90fc
Arjel1031 <arjel.bautista123@gmail.com>
2026-01-26 15:47:22 +0800

    Add project files.

---
+++ Model/ToDoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDo_List_ASP.Model
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public bool isDone { get; set; }
+
+
+    }
+}
